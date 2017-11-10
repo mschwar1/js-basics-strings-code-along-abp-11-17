6af4e20221b775ce2de05959454ab2d8ad490f99
Mai Schwartz <mai.schwartz@gmail.com>
2017-11-10 20:44:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 // Write your code in this file!
 const currentUser = "Rosemary Perfect Angel Dog"
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
